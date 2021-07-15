f4b20e2dc4210d9d2de791155b0f3231d7180c6b
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 16:52:05 -0700

    Update Kaggle_Netflix_Top_10_CSV.py

--- CSV_Scrapers/Kaggle_Netflix_Top_10_CSV.py
+++ CSV_Scrapers/Kaggle_Netflix_Top_10_CSV.py
@@ -1,7 +1,10 @@
 import csv
-
 import pandas as pd
+
+#Gets the csv file where it is in the computer
 df = pd.read_csv (r"C:\Users\jantisseril\Downloads\netflix_list.csv")
+
+#Prints top 10 Netflix titles
 counter = 1
 for row in df.get('title'):
     if counter < 11:

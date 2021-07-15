b686ec3ee2daf4b0095228db727a5b11bea84a7b
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 16:57:07 -0700

    Update Five9_Awards_HTML.py

--- HTML_Scrapers/Five9_Awards_HTML.py
+++ HTML_Scrapers/Five9_Awards_HTML.py
@@ -1,7 +1,10 @@
 import requests
-#import urllib.request
 from bs4 import BeautifulSoup
+
+#Gets the request url and gets the contents of the awards page
 recieved = requests.get('https://www.five9.com/about/awards').content
 soup = BeautifulSoup(recieved, "html.parser")
+
+#Finds where all the awards are listed and gets each award and prints it
 for i in soup.find_all('h4',{'class':'mt-3 pt-1 mb-2 pb-2 font-weight-bold text-primary'}):
     print(i.text.strip())

126348e935c4489d8545fee95e39e0ef50476341
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 16:50:40 -0700

    Update Kaggle_2019_DOJ_crimes_By_Race_CSV.py

--- CSV_Scrapers/Kaggle_2019_DOJ_crimes_By_Race_CSV.py
+++ CSV_Scrapers/Kaggle_2019_DOJ_crimes_By_Race_CSV.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 #Gets the csv from where it is in the computer
 df = pd.read_csv (r"C:\Users\jantisseril\Downloads\2019_DOJ_crimes_by_Race.csv")
-counter = 1
+
+#Keeps a record of total crimes committed by each race.
 race = {'w':0,'aa':0,'ai':0,'a':0}
 total = 0
+
+#Goes column by column and adds the crimes up by each group
 for col in df:
     for row in df.get(col):
         if col == 'All races':
@@ -20,8 +23,12 @@ for col in df:
             race['a'] += row
         else:
             continue
+            
+#Gets the highest number or crimes committed
 maxval = max(race.values())
 print("According to the 2019_DOJ_crimes_by_Race.csv, out of " + str(total) + " crimes, the group with the highest crime rate were ", end='')
+
+#Goes through each element in race and gets the group to be printed
 for r in race:
     if race[r] == maxval:
         if r == 'w':
@@ -34,4 +41,6 @@ for r in race:
             print("Asians",end='')
         else:
             continue
+            
+#Prints the percent of the total amount of crime committed by this group.
 print(" and made up " + str((maxval/total)*100) + "% of all crimes.")

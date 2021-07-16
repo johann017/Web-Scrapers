8bfb2c98aa989758356fe3ad078b8fcce19b394f
Johann Antisseril <57604319+johann017@users.noreply.github.com>
2021-07-15 17:00:55 -0700

    Update Top_Free_And_Paid_Apps_From_Apple_Appstore_HTML.py

--- HTML_Scrapers/Top_Free_And_Paid_Apps_From_Apple_Appstore_HTML.py
+++ HTML_Scrapers/Top_Free_And_Paid_Apps_From_Apple_Appstore_HTML.py
@@ -1,11 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+
+#Gets the requested url and gets the contents of the page
 recieved = requests.get('https://apps.apple.com/us/story/id1535572713').content
 soup = BeautifulSoup(recieved, "html.parser")
 print("Top Paid and Free App (via App Store)\n-------------------------------------")
+
+#Finds where the paid and free apps are
 for i in soup.find_all('h2', {'class':'section__headline'}):
+    
+    #Prints whether it's free or paid
     print(i.text.strip() + ":")
     counter = 1
+    
+    #Prints the apps under each section in the order it's displayed on the appstore
     for j in i.find_next().find_all('h3',{'class': 'truncate-single-line we-product-collection__item__product-name', 'dir': 'auto'}):
         print("\t" + str(counter) + " " + j.text.strip())
         counter += 1
